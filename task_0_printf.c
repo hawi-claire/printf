7a9c00fcc2bf39675624922ad9da9955501b2562
hawi-claire <7hawi7@gmail.com>
2023-11-18 06:08:26 -0800

    printf project

---
+++ task_0_printf.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * Printf - returns number of characters in a string
+ * Return: 0
+ */
+int _printf(const char *format, ...);
+{
+	printf("%d", (printf("All cows eat grass")));
+	return (0);
+}
